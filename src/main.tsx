5cf051a36dbb2edf8b570aafb15f9968f849343d
menga1001 <davidmen18diciembre@gmail.com>
2026-08-27 15:36:44 -0500

    Force cache bust: new JS hash, auto-clear old localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const SAVE_KEY = 'isla-emociones-save';
+const VERSION_KEY = 'isla-emociones-version';
+const CURRENT_VERSION = '1.0.1';
+
+const savedVersion = localStorage.getItem(VERSION_KEY);
+if (savedVersion !== CURRENT_VERSION) {
+  localStorage.removeItem(SAVE_KEY);
+  localStorage.setItem(VERSION_KEY, CURRENT_VERSION);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
